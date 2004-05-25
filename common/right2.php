089c807fe7eee489cf959cd61770091dd0afd60f
omnest <omnest@33270d1c-0d19-0410-869e-e3a17401575d>
2004-05-25 09:26:30 +0000

    added link to Licensing page
    git-svn-id: https://omnest.com/svn/omnestweb/trunk@4 33270d1c-0d19-0410-869e-e3a17401575d

--- common/right2.php
+++ common/right2.php
@@ -81,6 +81,25 @@
 		<!-- RIGHT 1 : End -->
 		<!-- news item : end-->
 
+		<!-- news item -->
+		<!-- Red Line -->
+		<table width="144" border="0" cellspacing="0" cellpadding="0" bgcolor="#E34030"><tr><td>
+		<img src="images/pixel.gif" width="144" height="1" alt="" border="0"><br>
+		</td></tr></table>
+
+		<!-- RIGHT 1 : Start -->
+
+		<table width="144" border="0" cellspacing="0" cellpadding="0" bgcolor="#FFFFFF"><tr><td valign="top" class="Small_Text">
+
+		<img src="images/pixel.gif" width="1" height="2" alt="" border="0"><br>
+                	<p><a href="licensing.php">Licensing options</a>
+		<br>
+		</td></tr></table>
+		<br/>
+
+		<!-- RIGHT 1 : End -->
+		<!-- news item : end-->
+
 	</td>
 
 <!-- RIGHT : End -->
