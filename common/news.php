dc2d377a2c5d343472aa88bde35f4ead9041b2a6
omnest <omnest@33270d1c-0d19-0410-869e-e3a17401575d>
2004-08-21 11:44:42 +0000

    Added infotek pressrelease
    git-svn-id: https://omnest.com/svn/omnestweb/trunk@32 33270d1c-0d19-0410-869e-e3a17401575d

--- common/news.php
+++ common/news.php
@@ -1,6 +1,26 @@
 				<table width="100%" border="0" cellspacing="13"><tr><td>
 					<img src="images/news_separator.gif" width="122" height="5" alt=""><br>
 					<div class="NewsDate">
+					August 21<sup>st</sup> 2004<br>
+					</div>
+					<div class="NewsPreview">
+					<a href="show.php?page=pressinfotekevents" class="NewsPreview">
+					Events in Taiwan.<br>
+					</a>
+					</div>
+
+					<img src="images/news_separator.gif" width="122" height="5" alt=""><br>
+					<div class="NewsDate">
+					August 21<sup>st</sup> 2004<br>
+					</div>
+					<div class="NewsPreview">
+					<a href="show.php?page=pressinfotek" class="NewsPreview">
+					Partnership with InfoTek Information Systems Co., Inc. of Taipei.<br>
+					</a>
+					</div>
+
+					<img src="images/news_separator.gif" width="122" height="5" alt=""><br>
+					<div class="NewsDate">
 					May 1<sup>st</sup> 2004<br>
 					</div>
 					<div class="NewsPreview">
@@ -19,6 +39,7 @@
 					</a>
 					</div>
 
+<!--
 					<img src="images/news_separator.gif" width="122" height="5" alt=""><br>
 					<div class="NewsDate">
 					April 15<sup>th</sup> 2003<br>
@@ -29,6 +50,7 @@
 					</a>
 					</div>
 
+-->
 					<img src="images/news_separator.gif" width="122" height="5" alt=""><br>
 				</td></tr></table>
 
