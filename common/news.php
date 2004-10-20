5adaee3eb833b0dbbdc8b469a5fc5431d7b69f06
omnest <omnest@33270d1c-0d19-0410-869e-e3a17401575d>
2004-10-20 11:11:54 +0000

    *** empty log message ***
    git-svn-id: https://omnest.com/svn/omnestweb/trunk@52 33270d1c-0d19-0410-869e-e3a17401575d

--- common/news.php
+++ common/news.php
@@ -1,6 +1,16 @@
 				<table width="100%" border="0" cellspacing="13"><tr><td>
 					<img src="images/news_separator.gif" width="122" height="5" alt=""><br>
 					<div class="NewsDate">
+					October 20<sup>st</sup> 2004<br>
+					</div>
+					<div class="NewsPreview">
+					<a href="show.php?page=download" class="NewsPreview">
+					Download OMNEST Demo <img src="images/arrow_anim.gif" width="75" height="25" alt=""><br>
+					</a>
+					</div>
+
+					<img src="images/news_separator.gif" width="122" height="5" alt=""><br>
+					<div class="NewsDate">
 					August 21<sup>st</sup> 2004<br>
 					</div>
 					<div class="NewsPreview">
