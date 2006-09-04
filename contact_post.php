6767d40dd4c67ecc650918e5340817b4c047bd9b
(no author) <(no author)@33270d1c-0d19-0410-869e-e3a17401575d>
2006-09-04 15:22:06 +0000

    git-svn-id: https://omnest.com/svn/omnestweb/trunk@165 33270d1c-0d19-0410-869e-e3a17401575d

--- contact_post.php
+++ contact_post.php
@@ -40,7 +40,8 @@ $architecture_verification = $_POST['architecture_verification'];
 $discrete_event_simulation = $_POST['discrete_event_simulation'];
 $embedding = $_POST['embedding'];
 $message = $_POST['message'];
-$headers = "From: Omnest web contact form on behalf of $name <$email>\n\r"; 
+$headers = "From: Omnest web contact form on behalf of $name <$email>\r\n".
+           "Reply-To: $name <$email>"; 
 if(eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)) {
 
   if(mail("contact@omnest.com","Omnest web contact","Name: $name". $eol .
