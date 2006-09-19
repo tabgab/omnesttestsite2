4a9ce07f7bf62c21dd8d66058a2c50ac575d7727
(no author) <(no author)@33270d1c-0d19-0410-869e-e3a17401575d>
2006-09-19 15:19:16 +0000

    git-svn-id: https://omnest.com/svn/omnestweb/trunk@189 33270d1c-0d19-0410-869e-e3a17401575d

---
+++ show.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title>Simulcraft Inc.</title>
+	<meta name="robots" content="INDEX,FOLLOW" />
+	<meta name="revisit-after" content="30" />
+	<meta name="description" content="OMNEST - an Embeddable Discrete Event Simulator Network" />
+	<meta name="keywords" content="embeddable discrete event simulator simulation embedding c++ c open source network"  />
+	<link rel="stylesheet" type="text/css" href="common/omnest.css">
+
+<meta http-equiv="refresh" content="0;url=/">
+</head>
+
+<body>
+</body>
+</html>
