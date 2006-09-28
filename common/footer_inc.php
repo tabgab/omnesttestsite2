70e7df6a15d56b6cb221882687526972a018d1a8
(no author) <(no author)@33270d1c-0d19-0410-869e-e3a17401575d>
2006-09-28 10:39:52 +0000

    git-svn-id: https://omnest.com/svn/omnestweb/trunk@196 33270d1c-0d19-0410-869e-e3a17401575d

--- common/footer_inc.php
+++ common/footer_inc.php
@@ -7,3 +7,13 @@
 _uacct = "UA-240922-1";
 urchinTracker();
 </script>
+<!-- MS adcenter -->
+<script>
+ microsoft_adcenterconversion_domainid = 46030;
+ microsoft_adcenterconversion_cp = 5050;
+</script>
+<script src="https://adcenter.microsoft.com/scripts/microsoft_adcenterconversion.js"></script>
+<noscript><img width="1" height="1" src="https://46030.r.msn.com/?type=1&cp=1"/></noscript>
+<!--
+<a href="http://advertising.msn.com/MSNadCenter/LearningCenter/adtracker.asp" target="_blank">::adCenter::</a>
+-->
