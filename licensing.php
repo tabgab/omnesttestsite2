e16618d44349140b46178e8e4f15bd253e2ba5ea
com <com@33270d1c-0d19-0410-869e-e3a17401575d>
2010-09-03 13:58:34 +0000

    licensing options (again)
    git-svn-id: https://omnest.com/svn/omnestweb/trunk@286 33270d1c-0d19-0410-869e-e3a17401575d

--- licensing.php
+++ licensing.php
@@ -26,23 +26,56 @@
 	<!-- Start Content -->
 	<div id="content">
 
-			<div id="header"><h1>Licensing Options</h1></div>
+<div id="header"><h1>Licensing Options</h1></div>
 
 
-<p>If you are looking into using OMNEST for simulation projects within your company, or you searching for an embeddable simulation kernel for your product, please check the <a href="licensingfaq.php">Licensing FAQ</a> and the license conditions below, and contact us so that we can provide you with further details.</p>
+<p>If you are looking into using OMNEST for simulation projects within your company,
+or you are searching for an embeddable simulation kernel for your product,
+please check the <a href="licensingfaq.php">Licensing FAQ</a> and the license
+conditions below. You are welcome to contact us with your questions,
+or for further details.</p>
 
-<p>Commercial licenses come in two basic forms. Perpetual licenses are available, and can be used indefinitely, with the option to upgrade if a major release becomes available. We have also recently introduced the option to rent licenses at a flat rate for 12 months. This will allow for the flexible configuration of licenses used, while also reducing the initial investment necessary to start using OMNEST in commercial endeavors.</p>
+<p>Commercial licenses come in two basic forms. Perpetual licenses are available,
+and can be used indefinitely, with the option to upgrade if a major release
+becomes available. Annual licenses can be purchased at a flat rate, and are
+valid for 12 months. Annual licenses enable a flexible configuration of licenses,
+while also reducing the initial investment necessary to start using OMNEST
+in commercial endeavors.</p>
 
 <h2>Standard licensing</h2>
-<p>We offer seat licenses.</p>
 
-<h2>One-year lease option</h2>
-<p>We offer to deduct the last 12 months' rental fees from the price, if an existing rental license is later converted to a perpetual license. This effectively means that you can start using OMNEST at a much lower price than standard licenses, and recover the first year's rental fee when you convert to perpetual licenses.</p>
+<p>We offer designated user licenses, which are similar to seat licenses,
+but are assigned to a single developer. Licenses can be re-designated to
+new individuals to accommodate for internal team changes on a reasonable time
+scale as long as this is not intended to defeat the concept that each user
+shall have their own separate license. OMNEST may be installed on a reasonable
+number of machines provided that the Named User is the only individual permitted
+to use it under both the perpetual and annual license terms.</p>
 
-<p>The option of prolongation of the rental is also available, and you can extend or reduce the number of licenses rented as you please. Rental of less than 12 months is not on offer, and our discount offer is limited to the last 12 months of rental fees only.</p>
+<h2>Annual licenses</h2>
+
+<p>We offer to deduct the last 12 months' license fees from the price,
+if an existing annual license is later converted to a perpetual license.
+This effectively means that you can start using OMNEST at a much lower price
+than standard licenses, and recover the first year's license fee when you
+convert to perpetual licenses.</p>
+
+<p>The option of prolongation of the annual license is also available,
+and you can extend or reduce the number of licenses used as you please.
+Licenses for terms shorter than 12 months are not offered, and our discount
+offer is limited to the last 12 months of annual license fees only you paid
+for the license to be converted only.</p>
 
 <h2>Redistribution</h2>
-<p>Both Rental and Perpetual licenses carry the option to redistribute your simulation code, NED files, etc. All IP you create in OMNEST is yours to distribute as you please. The licenses do not permit the distribution of self-running simulations however, since these must contain a part of OMNEST to function. We refer to these as "derivative products", and must consider their application in detail (scope, planned number, distribution, etc.) if you plan to use them extensively. Note that if your counterpart (or colleague) also has access to OMNEST, then the distribution of binaries is usually not necessary.</p>
+
+<p>Both annual and perpetual licenses carry the option to redistribute
+your simulation code, NED files, etc. All IP you create in OMNEST is yours
+to distribute as you please. The licenses do not permit the distribution
+of self-running simulations however, since these must contain a part of
+OMNEST to function. We refer to these as "derivative products", and must
+consider their application in detail (scope, planned number, distribution, etc.)
+if you plan to use them extensively. Note that if your counterpart (or colleague)
+also has access to OMNEST, then the distribution of binaries is usually not necessary.</p>
 
 We hope the above has met with your requirements. Please do not hesitate to <a href="contact.php">contact us</a> for details.
 
