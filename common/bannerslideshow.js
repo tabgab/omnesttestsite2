5b2e0d7604b3f82874c39cac8322d5ca384dbf94
Levente Mészáros <levy@omnetpp.org>
2013-05-27 16:40:43 +0200

    Added some new slideshow images.

--- common/bannerslideshow.js
+++ common/bannerslideshow.js
@@ -8,13 +8,15 @@ var mygallery=new fadeSlideShow({
     wrapperid: "bannerslideshow", //ID of blank DIV on page to house Slideshow
     dimensions: [340, 218], //width/height of gallery in pixels. Should reflect dimensions of largest image
     imagearray: [
-        ["images/slideshow/seq1.png", "", "", ""],
-        ["images/slideshow/ned1.png", "", "", ""],
-        ["images/slideshow/elog1.png", "", "", ""],
-        ["images/slideshow/largenet.png", "", "", ""],
-        ["images/slideshow/tcp.png", "", "", ""]        // NO COMMA HERE
+        ["images/slideshow/airport.jpg", "", "", ""],
+        ["images/slideshow/cell-tower.jpg", "", "", ""],
+        ["images/slideshow/mobile.jpg", "", "", ""],
+        ["images/slideshow/network-hub.jpg", "", "", ""],
+        ["images/slideshow/satellite-antenna.jpg", "", "", ""],
+        ["images/slideshow/surveillance.jpg", "", "", ""],
+        ["images/slideshow/system-on-chip.jpg", "", "", ""]        // NO COMMA HERE
     ],
-    displaymode: {type:'auto', pause:8000, cycles:0, wraparound:false},
+    displaymode: {type:'auto', pause:1000, cycles:0, wraparound:false},
     persist: false, //remember last viewed slide and recall within same session?
     fadeduration: 500, //transition duration (milliseconds)
     descreveal: "ondemand",
