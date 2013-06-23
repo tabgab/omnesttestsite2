6d7ad6b74154aeb921fc8cd0ba20bfaf7b6b7c6a
Rudolf Hornig <rudi@omnetpp.org>
2013-06-23 10:50:43 +0200

    added some docu for the toggle script

--- common/collapsible.js
+++ common/collapsible.js
@@ -1,3 +1,16 @@
+
+// Usage: 
+// - specify a toggle="toggleId" attribute on any element you want to turn into a toggle
+//   (toggleId is a unque identifier on the page) use toggle="all" to toggle all elements 
+//   on the page at the same time.
+// - specify class="toggle-expanded toggleId" for elements that should be visible in expanded state
+// - specify class="toggle-collapsed toggleId" for elements that should be visible in collapsed state
+// - specify class="toggle-slide toggleId" similar to the toggle-expanded, but this one animates the element
+//
+// TODO: - the toggle all function is a bit strange if some of the elements are expanded while others collapsed.
+//         Probaly should first expand all the element (and not toggle)
+//       - It might be better to hide/show the expanded/colapsed elements only at the end of the sliding animation
+//
 $(document).ready(function(){
   $(".toggle-expanded,.toggle-slide").css("display","none");
   $("[toggle]").click(function(event){
