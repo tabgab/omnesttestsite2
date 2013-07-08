7a271bb120ba74f8fc78934f5df2635cde7b41c9
Andras Varga <andras@omnetpp.org>
2013-07-08 12:46:40 +0200

    omnest-is.php: title case

--- omnest-is.php
+++ omnest-is.php
@@ -35,7 +35,7 @@ print_next_link_small($touritems[0]["link"], "Take the Product Tour!");
 the "OMNEST is" prefix into each one of them -->
 
 <img class="pic left" src="images/tour/network.png" width="200"> <!-- network? -->
-<h1>a network simulator platform</h1>
+<h1>A Network Simulator Platform</h1>
 <p class="righttext">It provides generic infrastructure for creating, running and evaluating simulations.
 OMNEST can cater for the simulation of communication networks including
 wired and wireless networks, mobile ad-hoc networks and sensor networks;
@@ -45,33 +45,33 @@ and for much more.</p>
 <div style="clear:both"></div>
 
 <img class="pic right" src="images/tour/research.png" width="200">
-<h1>for R&amp;D</h1>
+<h1>For R&amp;D</h1>
 <p class="lefttext">Researchers and R&amp;D people need to investigate deeper than just parameterizing existing systems.
 Expect frameworks and open-source tweakable models instead of "canned" models.</p>
 <div style="clear:both"></div>
 
 <img class="pic left" src="images/tour/swiss-army-knife2.png" width="200"> <!-- swiss army knife -->
-<h1>flexible</h1>
+<h1>Flexible</h1>
 TODO: COMPONENT ARCHITECTURE: MODELS CAN BE COMBINED FREELY.
 <p class="righttext">You can explore, modify and enhance simulation models, because you have access to model source and platform infrastructure.
 You can also get the simulator work together with other software in your toolbox: external simulators, Matlab, SystemC, HLA, you name it.</p>
 <div style="clear:both"></div>
 
 <img class="pic right" src="images/tour/fighter.png" width="200">  <!-- car? fighter plane? -->
-<h1>high performance</h1>
+<h1>High Performance</h1>
 <p class="lefttext">Models are written in C++, and execute on top of a streamlined simulation kernel to provide high event throughput.
 Diagnostic and animation features pose minimal overhead when not in use.</p>
 <div style="clear:both"></div>
 
 <img class="pic left" src="images/tour/trusted2.png" width="200">  <!-- tick? -->
-<h1>trusted by industry leaders</h1>
+<h1>Trusted by Industry Leaders</h1>
 <p class="righttext">Many large companies are relying on OMNEST in their R&amp;D for exploring large-scale
 problems using network simulation. Several have created their own internal simulation models
 or model libraries that they can turn to whenever new challenges appear.</p>
 <div style="clear:both"></div>
 
 <img class="pic right" src="images/tour/open2.png" width="200"> <!-- open door? open book? open box? palms? -->
-<h1>open</h1>
+<h1>Open</h1>
 <p class="lefttext">Open culture, open interfaces! OMNEST has started as an open-source project, and it shows. We do not hide any
 source code from you or lock down any part of the product. Well-documented extension APIs, plain-text input
 and output file formats make it easy to accommodate special needs.
@@ -79,7 +79,7 @@ TODO: supports nearly all platforms</p>
 <div style="clear:both"></div>
 
 <img class="pic left" src="images/tour/community2.png" width="200"> <!-- graduation hat? kemcso+lombik? atom? community/users? einstein? -->
-<h1>well embedded in the scientific community</h1>
+<h1>Well Embedded in the Scientific Community</h1>
 <p class="righttext">Under the name OMNeT++, OMNEST is a well-established tool in the scientific community.
 Over 200 papers are published each year at various conferences and in journals, and
 dedicated workshops take place annually with peer-reviewed submissions. The community mailing list
@@ -89,7 +89,7 @@ For you, this means you have access to a huge pool of talent and a wealth of inf
 <div style="clear:both"></div>
 
 <img class="pic right" src="images/tour/books.png" width="200">  <!-- community icon? github icon? open source logo? -->
-<h1>wealth of open-source simulation models</h1>
+<h1>A Wealth of Open-Source Simulation Models</h1>
 <p class="lefttext">As a result of the strong user community, there is an ever-growing number of open-source simulation models
 available to you, covering very diverse domains from internet routing to ad-hoc and sensor networks, in-car networks,
 4G, photonic on-chip networks and so on.
